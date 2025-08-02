6b8802ad117eb795ae008920b59b7d88ffbea4fc
Cereny <56936296+cerenyilmazjinx@users.noreply.github.com>
2025-08-02 21:56:24 +0700

    Add main.go
    Co-authored-by: Hajigur <66867581+hajigur69@users.noreply.github.com>

---
+++ cmd/cygnuscore/main.go
@@ -0,0 +1,23 @@
+// cmd/cygnuscore/main.go
+package main
+
+import (
+	"flag"
+	"log"
+	"os"
+
+	"cygnuscore/internal/cygnuscore"
+)
+
+func main() {
+	verbose := flag.Bool("verbose", false, "Enable verbose logging")
+	input := flag.String("input", "", "Input file path")
+	output := flag.String("output", "", "Output file path")
+	flag.Parse()
+
+	app := cygnuscore.NewApp(*verbose)
+	if err := app.Run(*input, *output); err != nil {
+		log.Printf("Error: %v", err)
+		os.Exit(1)
+	}
+}
